2d7ad7744ae25cc4701ca3e1481f05982ca394be
leviongit <levi.duncan.self@gmail.com>
2024-06-06 00:38:38 +0200

    collisons

--- app/main.rb
+++ app/main.rb
@@ -10,6 +10,11 @@ $player_default_jump = 15 * $default_gravity_coef**0.5
 
 $gtk.disable_framerate_warning!
 
+def generate_ff(begin_as = false)
+  state = begin_as
+  ->(toggle) { state ^= (toggle ? true : false) }
+end
+
 module GTK
   module Notify
     def tick_notification(...)
@@ -47,6 +52,8 @@ def init
     **spd
   }
 
+  $slowmo_ff = generate_ff
+
   $level_geometry = [
     {
       x: 640,
@@ -81,10 +88,18 @@ def init
       **spd
     },
     {
-      x: 120,
+      x: 150,
       y: 360,
-      w: 20,
+      w: 60,
       h: 540,
+      path: :pixel,
+      **spd
+    },
+    {
+      x: 500,
+      y: 360,
+      w: 200,
+      h: 100,
       path: :pixel,
       **spd
     }
@@ -105,7 +120,6 @@ end
 def ap_fric(obj, fric = 0.2)
   rv = 1 - fric
   obj.vx *= rv
-  # obj.vy *= rv
 end
 
 def collision_vert?(o1, o2, _tol = 1)
@@ -130,7 +144,7 @@ def player_can_wall_grapple?(onto)
 
   vx = player.vx.abs
   vx = vx < 1 ? 1 : vx
-  
+
   player.w += vx
   player.h -= 10
 
@@ -158,15 +172,21 @@ end
 
 def handle_player
   player = $player
+  player.x += player.vx
+  player.y += player.vy
 
-  ap_grav(player)
-  ap_vel(player)
-  ap_fric(player, 0.1)
+  py = player.y
+  px = player.x
+  ph = player.h
+  pw = player.w
 
-  opy = player.y
-  opx = player.x
-  oph = player.h
-  opw = player.w
+  anchor_x = player.anchor_x
+  player_left = px - anchor_x * pw
+  player_right = px + (1 - anchor_x) * pw
+
+  anchor_y = player.anchor_y
+  player_bot = py - anchor_y * ph
+  player_top = py - (1 - anchor_y) * ph
 
   player.y += (vy = player.vy) / 2
   player.x += (vx = player.vx) / 2
@@ -174,44 +194,53 @@ def handle_player
   player.w += vx.abs
 
   walls = $geometry.find_all_intersect_rect(player, $level_geometry)
-  player.y = opy
-  player.x = opx
-  player.h = oph
-  player.w = opw
-  return unless walls.length > 0
+  player.y = py
+  player.x = px
+  player.h = ph
+  player.w = pw
 
+  # return if walls.empty?
+
+  # :nodoc:
   dx = dy = Float::INFINITY
-  
 
   sty = {}
   stx = {}
 
   wally = walls.min_by { |w|
-    sy = opy <=> w.y
-    dyc = ((sty[w] = w.y + (((w.h * (1 - w.anchor_y)) + (player.h * player.anchor_y)) * sy)) - opy).abs
+    sy = py <=> w.y
+    dyc = ((sty[w] = w.y + (((w.h * (1 - w.anchor_y)) + (ph * anchor_y)) * sy)) - py).abs
     dy = dy < dyc ? dy : dyc
     dyc
   }
 
   wallx = walls.min_by { |w|
-    sx = opx <=> w.x
-    dxc = ((stx[w] = w.x + (((w.w * (1 - w.anchor_x)) + (player.w * player.anchor_x)) * sx)) - opx).abs
+    sx = px <=> w.x
+    dxc = ((stx[w] = w.x + (((w.w * (1 - w.anchor_x)) + (pw * anchor_x)) * sx)) - px).abs
     dx = dx < dxc ? dx : dxc
     dxc
   }
 
-  if wallx != wally
-    player.x = stx[wallx]
-    player.y = sty[wally]
-    player.vx = 0
-    player.vy = 0
-  elsif dx < dy
-    player.x = stx[wallx]
-    player.vx = 0
-  else
-    player.y = sty[wallx]
-    player.vy = 0
+  unless walls.empty?
+
+    if wallx == wally
+      if (dx + vx) < (dy + vy)
+        player.x = stx[wallx]
+        player.vx = 0
+      else
+        player.y = sty[wallx]
+        player.vy = 0
+      end
+    else
+      player.x = stx[wallx]
+      player.y = sty[wally]
+      player.vx = 0
+      player.vy = 0
+    end
   end
+
+  ap_grav(player)
+  ap_fric(player, $has_accelerated ? 0.1 : 0.2)
 end
 
 def input
@@ -222,24 +251,35 @@ def input
   player = $player
   pda = $player_default_accel
 
-  player.vx -= pda if keys_held.left
-  player.vx += pda if keys_held.right
+  if keys_held.left
+    player.vx -= pda
+    $has_accelerated = true
+  end
+  if keys_held.right
+    player.vx += pda
+    $has_accelerated = true
+  end
   if keys_down.up
     grappleable_wall = $level_geometry.filter { player_can_wall_grapple?(_1) }.min_by { (_1.x - player.x).abs }
     if grappleable_wall
       player.vy = $player_default_jump
       player.vx = (grappleable_wall.x <=> player.x) * -10
     else
-      player.vy = Math.log((player.vy < 0) ? $player_default_jump / 2 : player.vy + $player_default_jump, 1.2)
+      player.vy = Math.log(player.vy < 0 ? $player_default_jump / 2 : player.vy + $player_default_jump, 1.2)
     end
   end
+
+  if keys_down.q
+    $player.y = 410 + 24 # + 24
+    $player.x = 400 - 15
+  end
+
   player.vy -= pda * 2 if keys_held.down
-  
 
   $init_done = false if keys_down.r
-  return unless keys_held.forward_slash
+  return unless $slowmo_ff[keys_down.forward_slash]
 
-  $gtk.slowmo!(12)
+  $gtk.slowmo!(5)
 end
 
 def fill_renderq
@@ -251,11 +291,27 @@ def fill_renderq
     playertrail
   }
 
-  
-  
   $render_queue.concat(playertrails)
   $render_queue << player
   $render_queue.concat($level_geometry)
+
+  $render_queue.concat([{
+                         x: player.x,
+                         y: player.y,
+                         x2: $level_geometry[-1].x,
+                         y2: $level_geometry[-1].y,
+                         r: 255,
+                         primitive_marker: :line
+                       }, {
+                         x: player.x + player.vx,
+                         y: player.y + player.vy,
+                         x2: $level_geometry[-1].x,
+                         y2: $level_geometry[-1].y,
+                         b: 255,
+                         primitive_marker: :line
+                       }])
+
+  # $render_queue << {**$geometry.rect_props($player), r: 255, primitive_marker: :border}
 end
 
 # tick
@@ -271,6 +327,7 @@ def tick(args)
   fill_renderq
 
   outputs.primitives.concat($render_queue)
-  $render_queue = []
+  $render_queue.clear
+  $has_accelerated = false
   outputs.background_color = $bg_color
 end
